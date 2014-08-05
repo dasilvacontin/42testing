08000bca401f426b4e52199e2571d5bc6f2773b7
David DA-SILVA-CONTIN <dda-silv@e3r8p11.42.fr>
2014-08-05 19:02:10 +0200

    improved test case j04/ex02

--- j04/ex02/main.c
+++ j04/ex02/main.c
@@ -11,50 +11,61 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <time.h>
 
-#define GOES return printf("GOES ✓\n");
-#define FAKE_N_GAY return printf("FAKE AND GAY ✗\n")
+#define GOES return printf("\e[0;32mGOES ✓\e[0m\n");
+#define FAKE_N_GAY return printf("\e[0;31mFAKE AND GAY ✗\e[0m\n")
+#define SNAIL return printf("\e[0;33mYOU ARE TOO SLOW! ✗\e[0m\n")
+
+#define TEST_CASES 5
 
 int		ft_iterative_power(int nb, int power);
 
+void	ft_add_test_case(int *data, int nb, int power, int result)
+{
+	data[0] = nb;
+	data[1] = power;
+	data[2] = result;
+}
+
+void	ft_get_values(int *data, int *nb, int *power, int *actual)
+{
+	*nb = data[0];
+	*power = data[1];
+	*actual = data[2];
+}
+
 int		main(void)
 {
+	time_t 	start_t, end_t;
+	double	diff_t;
+	int data[TEST_CASES][3];
 	int nb;
 	int power;
 	int result;
 	int actual;
+	int i;
+	
+	ft_add_test_case(&data[0][0], -3, 0 ,1);
+	ft_add_test_case(&data[1][0], -235, -124, 0);
+	ft_add_test_case(&data[2][0], 21, 3, 9261);
+	ft_add_test_case(&data[3][0], 4, 10, 1048576);
+	ft_add_test_case(&data[4][0], 14, 8, 1475789056);
 
-	nb = -3;
-	power = 0;
-	result = ft_iterative_power(nb, power);
-	actual = 1;
-	printf("nb: %i, power: %i, result: %i, actual: %i\n", nb, power, result, actual);
-	if (result != actual)
-		FAKE_N_GAY;
-
-	nb = -235;
-	power = -124;
-	result = ft_iterative_power(nb, power);
-	actual = 0;
-	printf("nb: %i, power: %i, result: %i, actual: %i\n", nb, power, result, actual);
-	if (result != actual)
-		FAKE_N_GAY;
-
-	nb = 21;
-	power = 3;
-	result = ft_iterative_power(nb, power);
-	actual = 9261;
-	printf("nb: %i, power: %i, result: %i, actual: %i\n", nb, power, result, actual);
-	if (result != actual)
-		FAKE_N_GAY;
-
-	nb = 4;
-	power = 10;
-	result = ft_iterative_power(nb, power);
-	actual = 1048576;
-	printf("nb: %i, power: %i, result: %i, actual: %i\n", nb, power, result, actual);
-	if (result != actual)
-		FAKE_N_GAY;
-
-	GOES;	
+	i = 0;
+	while (i < TEST_CASES)
+	{
+		ft_get_values(&data[i][0], &nb, &power, &actual);
+		time(&start_t);
+		result = ft_iterative_power(nb, power);
+		time(&end_t);
+		diff_t = difftime(end_t, start_t);
+		printf("nb: %i, power: %i, result: %i, actual: %i, execution time = %f\n", nb, power, result, actual, diff_t);
+		if (result != actual)
+			FAKE_N_GAY;
+		if (diff_t >= 2.0)
+			SNAIL;
+		++i;
+	}
+	GOES;
 }
