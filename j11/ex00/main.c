5e8db8b72b965708e8646e9ec889d4cf7b7def2e
dasilvacontin <daviddasilvacontin@gmail.com>
2014-08-13 20:01:48 +0200

    main logs besides  return value

--- j11/ex00/main.c
+++ j11/ex00/main.c
@@ -6,9 +6,13 @@
 /*   By: dda-silv <dda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/08/13 19:22:30 by dda-silv          #+#    #+#             */
-/*   Updated: 2014/08/13 19:39:55 by dda-silv         ###   ########.fr       */
+/*   Updated: 2014/08/13 20:00:41 by dda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
+#define FAIL (printf("fail\n") || 1)
+#define GOES (printf("works!\n") && 0)
 
 #include "ft_list.h"
 t_list		*ft_create_elem(void *data);
@@ -20,5 +24,9 @@ int		main(void)
 
 	lol = 42;
 	elem = ft_create_elem(&lol);
-	return (elem->data != &lol);
+	if (elem->next != (void*)0)
+		return FAIL;
+	if (elem->data != &lol)
+		return FAIL;
+	return GOES;
 }
