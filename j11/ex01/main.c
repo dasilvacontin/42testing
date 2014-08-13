c0e7bc8613329b75e4ac0dfae1836a30e463bc77
dasilvacontin <daviddasilvacontin@gmail.com>
2014-08-13 20:45:42 +0200

    done testing for j11/ex01

--- j11/ex01/main.c
+++ j11/ex01/main.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/**************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
@@ -6,18 +6,21 @@
 /*   By: dda-silv <dda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/08/13 20:13:11 by dda-silv          #+#    #+#             */
-/*   Updated: 2014/08/13 20:27:28 by dda-silv         ###   ########.fr       */
+/*   Updated: 2014/08/13 20:38:04 by dda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#define FAIL (printf("fail\n") || 1)
+#define FAIL(x) (printf("fail: %s\n", x) || 1)
 #define GOES (printf("works!\n") && 0)
 
 #include "ft_list.h"
 
+void	ft_list_push_back(t_list **begin_list, void *data);
+
 int		main(void)
 {
+	t_list	**list;
 	t_list	*begin_list;
 	t_list	*curr;
 	int		i;
@@ -30,8 +33,17 @@ int		main(void)
 		curr->next = ft_create_elem((void*)&i);
 		curr = curr->next;
 	}
-	ft_list_push_back(&begin_list);
+	ft_list_push_back(&begin_list, &i);
 	if (!curr->next)
-		return FAIL;
+		return FAIL("didn't append to the end of the list");
+	if (curr->next->data != &i)
+		return FAIL("incorrect data in the elem");
+	curr = (void*)0;
+	list = &curr;
+	ft_list_push_back(list, &i);
+	if (!curr)
+		return FAIL("failed to add elem to empty list");
+	if (curr->data != &i)
+		return FAIL("wrong data in the elem");
 	return GOES;
 }
